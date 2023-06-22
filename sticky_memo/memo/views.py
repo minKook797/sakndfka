4511e7d8e003537dee5c54d5ee32d3bafd8df372
minKook797 <133008254+minKook797@users.noreply.github.com>
2023-06-22 19:10:42 +0900

    Add files via upload

---
+++ sticky_memo/memo/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, HttpResponse, redirect
+from memo.models import Memo
+
+def get_all_memo(request):
+    memo_list = Memo.objects.all()
+
+    context = {"memos":memo_list}
+    return render(request, "memo_list.html", context)
+
+def get_memo(request, id):
+    memo = Memo.objects.get(id=id)
+    context = {"memo": memo}
+
+    return render(request, "memo_detail.html", context)
+
+from memo.forms import MemoForm
+def create_memo(request):
+    if request.method == 'GET':
+        form = MemoForm()
+        context = {'form': form}
+        return render(request, 'memo_create.html', context)
+    
+    elif request.method == 'POST':
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('memo_list')
+
+
+def update_memo(request, id):
+    memo = Memo.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = MemoForm(request.POST, instance=memo)
+        if form.is_valid():
+            form.save()
+            return redirect('memo_list')
+        
+
+def delete_memo(request, id):
+    memo = Memo.objects.get(id=id)
+
+    if request.method == 'POST':
+        memo.delete()
+        return redirect('memo_list')
---
+++ sticky_memo/sticky_memo/urls.py
@@ -0,0 +1,34 @@
+"""sticky_memo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+from memo import views as memo_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # 특정 url ”my-home”에 들어가면 “hello world!”를 표시해보기
+    path('my-home/', views.my_home),
+    path('memo/', memo_views.get_all_memo, name="memo_list"),
+    path('', views.index, name='main'),
+    path('memo/<int:id>/', memo_views.get_memo, name='memo_detail'),
+    path('memo/create/', memo_views.create_memo, name="memo_create"),
+    path('memo/update/<int:id>/', memo_views.update_memo, name="memo_update"),
+    path('memo/delete/<int:id>/', memo_views.delete_memo, name="memo_delete")
+]
+ 
+
+
